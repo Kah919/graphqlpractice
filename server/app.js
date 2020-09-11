85dc1991f032e4c75a26ae4719e345523a9e2a01
Kah919 <kahyap13@gmail.com>
2020-09-11 15:39:02 -0400

    Create RootQuery in schema and exported it to app

--- server/app.js
+++ server/app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql'); // lets express understand graphQL
 const app = express();
 const port = 4000;
+const schema = require('./schema/schema');
 
 app.use('/graphql', graphqlHTTP({
-
+    schema
 }));
 
 app.listen(port, () => {
